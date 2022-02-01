786893ebca4732cb5530e9e7236137c83005312a
shyamsutar <shyamsutar629@gmail.com>
2022-02-01 10:22:11 +0530

    added gallery to Firebase

--- app/src/main/java/com/example/socialmediaapp/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/socialmediaapp/Fragment/ProfileFragment.java
@@ -13,10 +13,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.socialmediaapp.Adapter.FriendAdapter;
 import com.example.socialmediaapp.Model.FriendModel;
 import com.example.socialmediaapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
@@ -27,6 +34,9 @@ public class ProfileFragment extends Fragment {
     RecyclerView recyclerView;
     ArrayList<FriendModel> list;
     ImageView coverPhoto,profileImage;
+    FirebaseAuth auth;
+    FirebaseStorage storage;
+    FirebaseDatabase database;
 
 
     public ProfileFragment() {
@@ -39,6 +49,9 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
+
     }
 
     @Override
@@ -47,7 +60,9 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-
+        auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
         recyclerView = view.findViewById(R.id.friendRv);
 
         list = new ArrayList<>();
@@ -89,6 +104,21 @@ public class ProfileFragment extends Fragment {
         if(data.getData()!=null){
             Uri uri = data.getData();
             profileImage.setImageURI(uri);
+
+            final StorageReference reference = storage.getReference().child("cover_photo").child(FirebaseAuth.getInstance().getUid());
+            reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getContext(), "Cover Photo Saved", Toast.LENGTH_SHORT).show();
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            database.getReference().child("Users").child(auth.getUid()).child("coverPhoto").setValue(uri.toString());
+                        }
+                    });
+                }
+            });
+
         }
     }
 }
